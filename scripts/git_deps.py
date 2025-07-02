2d8eb2b903ef67808bba48731345f33bfaecdc1a
Leonardo Marcucci <l.marcucci@sintraconsulting.eu>
2025-07-02 12:43:02 +0200

    fix: Auto-generate cli entrypoints.

--- scripts/git_deps.py
+++ scripts/git_deps.py
@@ -15,17 +15,20 @@ def delete() -> None:
 
 def generate() -> None:
     deps = []
+    clis = {}
     for item in Path("tools").iterdir():
         if item.is_dir():
             deps.append(
                 f"{item.name} @ git+https://github.com/marcuson/python-scripts.git#subdirectory=tools/{item.name}"
             )
+            clis[item.name] = f"{item.name}.cli.main:cli"
 
     with open("pyproject.toml", "r", encoding="utf-8") as f:
         content = f.read()
 
     data = parse(content)
     data["project"]["dependencies"] = deps
+    data["project"]["scripts"] = clis
 
     with open("pyproject.toml", "w", encoding="utf-8") as f:
         f.write(dumps(data))
